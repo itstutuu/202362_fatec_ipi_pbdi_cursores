1425cb877135deb75d54b4f1708a19be0936f69b
Arthur Carvalho <arthurcaj@gmail.com>
2026-09-22 11:05:07 -0300

    learn(ap16-cursores): encerra bloquinho 2.5.1

--- script.sql
+++ script.sql
@@ -1,3 +1,4 @@
+-- Active: 1788268286026@@localhost@5433@yt_channels
 -- 1 database pro semestre inteiro
 CREATE TABLE tb_top_youtubers(
 	cod_top_youtubers SERIAL PRIMARY KEY,
@@ -10,3 +11,29 @@ CREATE TABLE tb_top_youtubers(
 	started INT NULL
 );
 
+ALTER TABLE tb_top_youtubers
+	ALTER COLUMN video_views TYPE BIGINT;
+
+SELECT * FROM tb_top_youtubers;
+
+-- Cursores - iterar as linhas da tabela para mostrar youtubers, fzd uma projeção da coluna
+DO $$
+DECLARE
+	-- 1. Decalração do cursor (unbound)
+	cur_nomes_youtubers REFCURSOR;
+	v_youtuber VARCHAR(200);
+BEGIN
+	-- 2. abertura do cursor
+	OPEN cur_nomes_youtubers FOR
+		SELECT youtuber
+			FROM
+			tb_top_youtubers;
+	LOOP
+		3. 
+		FETCH cur_nomes_youtubers INTO v_youtubers
+		EXIT WHEN NOT FOUND;
+		RAISE NOTICE '%', v_youtuber;
+	END LOOP;
+	CLOSE cur_nomes_youtubers;
+END;
+$$
